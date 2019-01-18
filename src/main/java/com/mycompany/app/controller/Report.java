c021514b630be89973f9c682f07ce3ca6a06e60b
daria-grebneva <greda2598@gmail.com>
2019-01-18 14:22:55 +0300

    Исправили работу с файлом - открытие, закрытие и вставка в файл теперь в одном методе #6

--- src/main/java/com/mycompany/app/controller/Report.java
+++ src/main/java/com/mycompany/app/controller/Report.java
@@ -4,33 +4,44 @@ import com.mycompany.app.linksHandler.BrokenLinksFinder;
 import com.mycompany.app.linksHandler.LinksFinder;
 
 import java.io.File;
-import java.io.FileNotFoundException;
 import java.io.PrintWriter;
 import java.util.ArrayList;
 import java.util.List;
 
 public class Report {
-    public Report(String filename) throws FileNotFoundException {
-        this.writer = new PrintWriter(new File(filename));
+    public Report(String filename) {
+        this.filename = filename;
     }
 
     public void append(List<Response> brokenLinks) {
-        for (Response brokenLink : brokenLinks) {
-            StringBuilder builder = new StringBuilder();
-            builder.append(brokenLink.getUrl());
-            builder.append(';');
-            builder.append(brokenLink.getStatusCode());
-            builder.append(';');
-            builder.append(brokenLink.getStatusMessage());
-            builder.append('\n');
-            this.writer.write(builder.toString());
-            this.writer.flush();
+        try {
+            if (filename == null || filename.isEmpty()) {
+                return;
+            }
+            this.writer = new PrintWriter(new File(filename));
+
+            for (Response brokenLink : brokenLinks) {
+                StringBuilder builder = new StringBuilder();
+                builder.append(brokenLink.getUrl());
+                builder.append(';');
+                builder.append(brokenLink.getStatusCode());
+                builder.append(';');
+                builder.append(brokenLink.getStatusMessage());
+                builder.append('\n');
+                this.writer.write(builder.toString());
+                this.writer.flush();
+            }
+        } catch (Exception ex) {
+            System.out.println(ex.getMessage());
+        } finally {
+            if (this.writer != null) {
+                this.writer.close();
+            }
         }
-        finalize();
     }
 
     public static List<Response> getBrokenLinks(List<String> pages, ReaderState state) throws Exception {
-        List<Response> brokenLinks = new ArrayList<Response>();
+        List<Response> brokenLinks = new ArrayList<>();
         LinksFinder linksFinder = new LinksFinder();
         for (String page : pages) {
             List<String> links = linksFinder.getLinks(page, state);
@@ -40,14 +51,11 @@ public class Report {
         return brokenLinks;
     }
 
-    public static void printBrokenLinks(List<Response> brokenLinks, String outputFile) throws FileNotFoundException {
+    public static void printBrokenLinks(List<Response> brokenLinks, String outputFile) {
         Report writer = new Report(outputFile);
         writer.append(brokenLinks);
     }
 
-    protected void finalize() {
-        this.writer.close();
-    }
-
     private PrintWriter writer;
+    private String filename;
 }
